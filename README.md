d0b78587afab5b9cdb02bfce41aaaeb30e2b090c
LinkWu <wjianl@qq.com>
2015-02-14 20:05:34 +0800

    test zip

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# golabs
-[![Build Status](https://travis-ci.org/wolfired/golabs.svg?branch=develop)](https://travis-ci.org/wolfired/golabs)   
+golabs
+=====================
+[![Build Status](https://travis-ci.org/wolfired/golabs.svg?branch=develop)](https://travis-ci.org/wolfired/golabs)
+
 This is my Golang labs
--- testbed/main.go
+++ testbed/main.go
@@ -1,9 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "archive/zip"
+import "bytes"
+import "os"
+
+import "fmt"
 
 func main() {
-	fmt.Println("c_arr")
+	b := bytes.Buffer{}
+
+	zip_file_buf := zip.NewWriter(&b)
+
+	zip_item, err := zip_file_buf.Create("phone.txt")
+
+	if nil == err {
+		zip_item.Write([]byte("18601011241"))
+		zip_file_buf.Close()
+
+		zip_file, err := os.Create("D:/phones.zip")
+		if nil == err {
+			zip_file.Write(b.Bytes())
+			zip_file.Close()
+		} else {
+			fmt.Println(err.Error())
+		}
+
+	} else {
+		fmt.Println(err.Error())
+	}
 }
