185c2359f8bdef649ac5599c2fba75434c5eab59
LinkWu <wjianl@qq.com>
2015-04-12 17:50:59 +0800

    add a daemon program for set home ip

---
+++ main/home/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	for true {
+		set_home_ip()
+		time.Sleep(15 * time.Minute)
+	}
+}
+
+func set_home_ip() {
+	resp, err := http.Get("https://ddns-deriflow.rhcloud.com//set_home_ip?passwd=112358")
+	if nil != err {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if nil != err {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(body))
+}
