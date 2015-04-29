5c57d0d038c706baed57c45c7c5e0a466a37f408
LinkWu <wjianl@qq.com>
2015-04-29 18:13:09 +0800

    none

--- main/sethomeip/main.go
+++ main/sethomeip/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"log"
+	// "log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	for true {
+	for 1 == len(os.Args) {
 		set_home_ip()
 		time.Sleep(5 * time.Minute)
 	}
@@ -16,7 +17,7 @@ func main() {
 func set_home_ip() {
 	_, err := http.Get("https://ddns-deriflow.rhcloud.com//set_home_ip?passwd=112358")
 	if nil != err {
-		log.Fatalln(err)
+		// log.Fatalln(err)
 	}
 
 	/*	defer resp.Body.Close()
