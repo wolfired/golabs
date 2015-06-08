154dab8be740d40269bebea20524cbec0abae91a
LinkWu <wjianl@qq.com>
2015-06-08 19:46:47 +0800

    some code

--- main/sethomeip/main.go
+++ main/sethomeip/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	passwd := os.Args[0]
-	key := os.Args[1]
-	domain := os.Args[2]
-	host := os.Args[3]
+	passwd := os.Args[1]
+	key := os.Args[2]
+	domain := os.Args[3]
+	host := os.Args[4]
 	url := fmt.Sprintf("https://ddns-deriflow.rhcloud.com/set_ip?passwd=%s&key=%s&domain=%s&host=%s", passwd, key, domain, host)
 	http.Get(url)
 }
