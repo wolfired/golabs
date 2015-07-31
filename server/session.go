ddfddfabb038b367b954e5e0e46b4eb432491193
LinkWu <wjianl@qq.com>
2015-07-31 22:43:10 +0800

    none

--- server/session.go
+++ server/session.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-
-	"github.com/wolfired/golabs/server/logic"
 )
 
 type Session struct {
@@ -18,17 +16,5 @@ func (s *Session) Setup(c net.Conn) *Session {
 }
 
 func (s *Session) Run() {
-	ra := s.conn.RemoteAddr().String()
 
-	bfio := bufio.NewReaderSize(s.conn, 1024)
-
-	for {
-		line, err := bfio.ReadString(0)
-		if nil == err {
-			logic.Saver[0]()
-			fmt.Println(ra, "->", line)
-		} else {
-			break
-		}
-	}
 }
