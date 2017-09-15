f26fe0380811c43d0c17272e21524451f9f3a58b
LinkWu <wjianl@qq.com>
2017-09-15 19:29:54 +0800

    变更链接

--- main/sethomeip/main.go
+++ main/sethomeip/main.go
@@ -10,8 +10,8 @@ func main() {
 	help := flag.Bool("help", false, "帮助")
 	passwd := flag.String("passwd", "", "密码")
 	key := flag.String("key", "", "标识键")
-	domain := flag.String("domain", "pi", "域")
-	host := flag.String("host", "wolfired.com", "主机")
+	domain := flag.String("domain", "wolfired.com", "域名")
+	host := flag.String("host", "pi", "主机")
 	flag.Parse()
 
 	if *help || "" == *passwd || "" == *key || "" == *domain || "" == *host {
@@ -19,6 +19,6 @@ func main() {
 		return
 	}
 
-	url := fmt.Sprintf("https://ddns-wolfired.rhcloud.com/set_ip?passwd=%s&key=%s&domain=%s&host=%s", *passwd, *key, *domain, *host)
+	url := fmt.Sprintf("http://sloot.wolfired.com/set_ip?passwd=%s&key=%s&domain=%s&host=%s", *passwd, *key, *domain, *host)
 	http.Get(url)
 }
