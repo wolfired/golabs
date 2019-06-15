78f129586a71e5019833440e741428d081a88545
LinkWu <wjianl@qq.com>
2019-06-15 14:07:27 +0000

    测试代码

--- testbed/main.go
+++ testbed/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,17 +14,120 @@ import (
 	"strings"
 	"time"
 
+	"github.com/wolfired/golabs/go2d/display"
+
 	"net/http"
 	"net/http/cookiejar"
 	_ "net/http/pprof"
 
+	"github.com/gen2brain/x264-go"
+	"github.com/wolfired/golabs/gotv/frame"
 	"github.com/wolfired/golabs/wasm/wabf"
 )
 
 func main() {
+	con0 := display.CreateContainer("Con0")
+
+	fmt.Printf("%p %p %p %p", con0, &con0.Object, con0.Object.IEntity, con0.Object.Child)
+
+	// frame.FromFile("./spritesheet_magicbubbles.png", 100, 100, -3) //.ToFiles("./magicbubbles")
+	// f, _ := os.Create("png.gzip")
+	// defer f.Close()
+	// f.Write(frame.FromFile("./spritesheet_magicbubbles.png", 100, 100, -3).FrameAt(0))
 	// gotv.Boot()
 	// goss.Boot()
 	// ptt.Boot()
+	// stage := display.CreateContainer("stage")
+	// obj0 := display.CreateObject("obj0")
+	// fmt.Println(obj0.Name(), stage.Name())
+	// stage.Add(obj0)
+	// ecs.CreateEntity("a").DryGet(comps.CCTransform).(comps.Transform).Exec()
+
+	// {
+	// 	rand.Seed(time.Now().UnixNano())
+	// 	bs := make([]byte, 320*288*4)
+
+	// 	for i := 0; i < len(bs); i++ {
+	// 		bs[i] = byte(rand.Intn(256))
+	// 	}
+
+	// 	g, _ := os.Create("time.gzip")
+	// 	defer g.Close()
+	// 	gw, _ := gzip.NewWriterLevel(g, gzip.BestCompression)
+	// 	gw.Write(bs)
+
+	// 	img := image.NewNRGBA(image.Rect(0, 0, 320, 288))
+
+	// 	for i := 0; i < len(bs); i++ {
+	// 		img.Pix[i] = bs[i]
+	// 	}
+
+	// 	p, _ := os.Create("time.png")
+	// 	defer p.Close()
+
+	// 	png.Encode(p, img)
+	// }
+
+	// testX264()
+}
+
+func testX264() {
+	buf := bytes.NewBuffer(make([]byte, 0))
+
+	wid := 100
+	hei := 100
+
+	opts := &x264.Options{
+		Width:     wid,
+		Height:    hei,
+		FrameRate: 24,
+		Tune:      "zerolatency",
+		Preset:    "veryfast",
+		Profile:   "baseline",
+		LogLevel:  x264.LogDebug,
+	}
+
+	enc, err := x264.NewEncoder(buf, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	img := x264.NewYCbCr(image.Rect(0, 0, opts.Width, opts.Height))
+	draw.Draw(img, img.Bounds(), image.Black, image.ZP, draw.Src)
+
+	a := frame.FromFile("./spritesheet_magicbubbles.png", 100, 100, -3)
+
+	for i := 0; i < 24*60; i++ {
+		raw := a.FrameAt(i%a.Len(), false)
+
+		offset := 0
+
+		for x := 0; x < wid; x++ {
+			for y := 0; y < hei; y++ {
+				img.Set(x, y, color.NRGBA{raw[offset+0], raw[offset+1], raw[offset+2], raw[offset+3]})
+			}
+		}
+
+		err = enc.Encode(img)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = enc.Flush()
+	if err != nil {
+		panic(err)
+	}
+
+	err = enc.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	f, _ := os.Create("x.264")
+	defer f.Close()
+
+	f.Write(buf.Bytes())
 }
 
 func testPt() {
