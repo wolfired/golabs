89b738645fe7d2b442b5c3820abc3a67bf00ee62
LinkWu <wjianl@qq.com>
2019-10-30 17:39:10 +0800

    代码调整

--- adobe/swfchef/swf_test.go
+++ adobe/swfchef/swf_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSwfRead(t *testing.T) {
-	s := SwfRead("C:\\Users\\Administrator\\Desktop\\res\\C.swf")
+	s := SwfRead("C:\\Users\\link\\Desktop\\test003.swf")
 
 	fmt.Printf("%s\n", s.Signature())
 	fmt.Printf("%d\n", s.Version())
@@ -14,6 +14,8 @@ func TestSwfRead(t *testing.T) {
 	fmt.Println(s.FrameSize())
 	fmt.Printf("%f\n", s.FrameRate())
 	fmt.Println(s.FrameCount())
+
+	s.Tags()
 
 	fmt.Println(raw2sbn([]byte{0x60, 0x00, 0x28, 0x00, 0x00, 0x28, 0x00}, 41, 12))
 	fmt.Println(raw2sbn([]byte{0xFF, 0xFE, 0xFD, 0xFC}, 8, 8))
