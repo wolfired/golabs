b53fc526ab3fb2c98eeda72a4ed52c802713e83b
LinkWu <wjianl@qq.com>
2021-01-07 14:59:58 +0800

    消除space

--- main/piong/main.go
+++ main/piong/main.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	help := flag.Bool("help", false, "Help")
-	mode := flag.String("mode", "client", "Mode: server or client")
+	mode := flag.String("mode", "", "Mode: server or client")
 	address := flag.String("address", "0.0.0.0:3980", "Address")
 	bsize := flag.Int("bsize", 4096, "Buf size")
 
 	flag.Parse()
 
-	if *help {
+	if *help || "" == *mode {
 		flag.Usage()
 		return
 	}
@@ -182,9 +183,9 @@ func client(address string, bsize int) {
 					fmt.Printf("%s input error: %s\n", time.Now().Format("2006-01-02 15:04:05"), err.Error())
 					continue
 				}
-
-				if 0 < len(buf[:n-1]) {
-					chanBuf <- buf[:n-1]
+				str := strings.TrimSpace(string(buf[:n]))
+				if 0 < len(str) {
+					chanBuf <- []byte(str)
 				}
 			}
 		}
