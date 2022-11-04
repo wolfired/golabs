0eddd5a2f9a61dd9f663652ee966f4a1addb1c28
LinkWu <wjianl@qq.com>
2022-11-04 20:02:44 +0800

    heroku收费，迁移sloot

---
+++ main/sloot/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/wolfired/golabs/www"
+)
+
+/*
+AUTH_PASSWD:
+GEN_NOES_HIGHT:
+GEN_NOES_LOW:
+HOST:
+NAMESILO_KEY:
+NAMESILO_TYPE:
+NAMESILO_VERSION:
+PORT:
+ZEROTIER_TOKEN:
+*/
+
+func main() {
+	www.Serve()
+}
--- www/index.go
+++ www/index.go
@@ -1,20 +1,18 @@
 package www
 
 import (
-	"strconv"
 	"fmt"
 	"net/http"
 	"os"
-
-	"github.com/wolfired/golabs/namesilo"
+	"strconv"
 )
 
 func init() {
-	Passwd = os.Getenv("AUTH_PASSWD")
-	
-	namesiloVersion, _ := strconv.Atoi(os.Getenv("NAMESILO_VERSION"))
-	SiloCli = &namesilo.SiloClient{Version: uint(namesiloVersion), Type: os.Getenv("NAMESILO_TYPE"), Key: os.Getenv("NAMESILO_KEY")}
-	
+	// Passwd = os.Getenv("AUTH_PASSWD")
+
+	// namesiloVersion, _ := strconv.Atoi(os.Getenv("NAMESILO_VERSION"))
+	// SiloCli = &namesilo.SiloClient{Version: uint(namesiloVersion), Type: os.Getenv("NAMESILO_TYPE"), Key: os.Getenv("NAMESILO_KEY")}
+
 	hight, _ := strconv.Atoi(os.Getenv("GEN_NOES_HIGHT"))
 	Hight = uint(hight)
 
@@ -23,9 +21,9 @@ func init() {
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "%s", "<a href='set_ip?passwd=&key=&domain=&host=' target='_blank'>记录IP</a><br/>")
-	fmt.Fprintf(res, "%s", "<a href='get_ip?key=' target='_blank'>获取IP</a><br/>")
-	fmt.Fprintf(res, "%s", "<a href='show_nts?key=' target='_blank'>下次更新IP的时间</a><br/>")
+	// fmt.Fprintf(res, "%s", "<a href='set_ip?passwd=&key=&domain=&host=' target='_blank'>记录IP</a><br/>")
+	// fmt.Fprintf(res, "%s", "<a href='get_ip?key=' target='_blank'>获取IP</a><br/>")
+	// fmt.Fprintf(res, "%s", "<a href='show_nts?key=' target='_blank'>下次更新IP的时间</a><br/>")
 	fmt.Fprintf(res, "%s", "<a href='gen_noes?noes=' target='_blank'>GenNoes</a>")
 }
 
@@ -34,9 +32,9 @@ Serve Web服务
 */
 func Serve() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("/set_ip", SetIP)
-	http.HandleFunc("/get_ip", GetIP)
-	http.HandleFunc("/show_nts", ShowNextTimestamp)
+	// http.HandleFunc("/set_ip", SetIP)
+	// http.HandleFunc("/get_ip", GetIP)
+	// http.HandleFunc("/show_nts", ShowNextTimestamp)
 	http.HandleFunc("/generate_204", Generate204)
 	http.HandleFunc("/gen_noes", GenNoes)
 
