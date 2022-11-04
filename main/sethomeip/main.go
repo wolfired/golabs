62155fd3897f09f019c4cf2f8237e28223ac3238
LinkWu <wjianl@qq.com>
2022-11-04 16:10:39 +0800

    namesilo cli

--- main/sethomeip/main.go
+++ main/sethomeip/main.go
@@ -2,32 +2,42 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/wolfired/golabs/namesilo"
 )
+
+var (
+	/*SiloCli namesilo客户端*/
+	SiloCli *namesilo.SiloClient
+)
+
+func init() {
+	namesiloVersion, _ := strconv.Atoi(os.Getenv("NAMESILO_VERSION"))
+	SiloCli = &namesilo.SiloClient{Version: uint(namesiloVersion), Type: os.Getenv("NAMESILO_TYPE"), Key: os.Getenv("NAMESILO_KEY")}
+}
 
 func main() {
 	help := flag.Bool("help", false, "帮助")
-	passwd := flag.String("passwd", "", "密码")
-	key := flag.String("key", "", "标识键")
+	ip := flag.String("ip", "", "公网IP")
 	domain := flag.String("domain", "wolfired.com", "域名")
 	host := flag.String("host", "pi", "主机")
-	dryrun := flag.Bool("dryrun", false, "只输出URL")
 	flag.Parse()
 
-	if *help || "" == *passwd || "" == *key || "" == *domain || "" == *host {
+	if *help || *ip == "" || *domain == "" || *host == "" {
 		flag.Usage()
 		return
 	}
 
-	url := fmt.Sprintf("http://sloot.wolfired.com/set_ip?passwd=%s&key=%s&domain=%s&host=%s", *passwd, *key, *domain, *host)
-	if *dryrun {
-		fmt.Println(url)
+	rr := SiloCli.DNSListRecords(*domain).GetResourceRecordByHost(*host + "." + *domain)
+	if nil == rr {
 		return
 	}
-	_, err := http.Get(url)
-	if nil != err {
-		fmt.Println(err.Error())
+
+	if rr.Value == *ip {
 		return
 	}
+
+	SiloCli.DNSUpdateRecord(*domain, rr.RecordID, *host, *ip, rr.Distance, rr.TTL)
 }
